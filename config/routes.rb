316a7a8f0891e337c7bfb572e67d91b677c8b7e1
Yassine Elabbassi <yassine@elabbassi.org>
2017-02-16 14:58:29 -0200

    cocktail controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+
+  # get 'cocktails', to: 'cocktails#index'
+
+  #  get 'cocktails/new', to: 'cocktails#new'
+  # post 'cocktails', to: 'cocktails#create'
+
+  # get 'cocktails/:id', to: 'cocktails#show', as: 'cocktail'
+
+
+
+  # get "cocktails/:id/doses/new", to: 'doses#new', as: 'cocktail_doses'
+  # post "cocktails/:id/doses", to: 'doses#create'
+
+  # delete 'doses/:id', to: 'dose#destroy'
+
+
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lime")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "sugar")
+Ingredient.create(name: "club soda")
+Ingredient.create(name: "coca cola")
+Ingredient.create(name: "whiskey")
+Ingredient.create(name: "cachaca")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "Bitter")
+
+Cocktail.create(name: 'cuba libre')
+Cocktail.create(name: 'mojito')
+Cocktail.create(name: 'caipirinha')
+Cocktail.create(name: 'old fashion')
+
+Dose.create(description: '4.5 cl', ingredient_id: 9, cocktail_id: 1)
+Dose.create(description: 'topped', ingredient_id: 6, cocktail_id: 1)
+
+Dose.create(description: '4', ingredient_id: 1, cocktail_id: 2)
+Dose.create(description: '2 to 3 teaspoons', ingredient_id: 4, cocktail_id: 2)
+Dose.create(description: '8 to 10 fresh', ingredient_id: 3, cocktail_id: 2)
+Dose.create(description: '6cl', ingredient_id: 9, cocktail_id: 2)
+
+
+Dose.create(description: '4 to 6', ingredient_id: 1, cocktail_id: 3)
+Dose.create(description: '1 tablespoon', ingredient_id: 4, cocktail_id: 3)
+Dose.create(description: '9 cl', ingredient_id: 8, cocktail_id: 3)
+
+Dose.create(description: '1/2 teaspoon', ingredient_id: 4, cocktail_id: 4)
+Dose.create(description: '2 dashes', ingredient_id: 10, cocktail_id: 4)
+Dose.create(description: '4.5 cl', ingredient_id: 7, cocktail_id: 4)

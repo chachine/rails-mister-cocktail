5063caf5193d86244a80a23ef9910349739be903
Yassine Elabbassi <yassine@elabbassi.org>
2017-02-17 14:42:19 -0200

    card design and photo handling

---
+++ app/uploaders/photo_uploader.rb
@@ -0,0 +1,22 @@
+# encoding: utf-8
+
+class PhotoUploader < CarrierWave::Uploader::Base
+  include Cloudinary::CarrierWave
+
+   process eager: true  # Force version generation at upload time.
+
+  process convert: 'jpg'
+
+  version :standard do
+    resize_to_fit 800, 600
+  end
+
+  version :thumb do
+    cloudinary_transformation  radius: 20,
+      width: 150, height: 150, crop: :thumb
+  end
+
+  version :card do
+    cloudinary_transformation width: 250, height: 200, gravity: :center
+  end
+end
